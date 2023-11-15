c696ca5e86ee462c571bbd103a599fdb38f29132
Juliana Zagurska <julianazagurska@Julianas-MacBook-Pro.local>
2023-11-15 22:32:24 +0100

    initital commit

---
+++ script.js
@@ -0,0 +1,19 @@
+'use sctrict';
+// Task 1 : find sum of all elements 
+function sumThemAll (...args){
+    let sum = 0;
+    for (let arg of args) sum += arg;
+    return sum;
+}
+console.log(sumThemAll(1,5,3,10));
+
+// Task 2 : The task of using closure
+function multiply(a) {
+    return function(b) {
+        return a * b;
+    };
+}
+const result = multiply(3)(4);
+console.log(result); 
+
+// Task 3 : sort movies array  
